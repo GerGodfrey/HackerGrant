4fbaec5713d9b851b63da6489f1c0f140a18b3f5
Gerardo Godfrey <ggc@Gerardos-MacBook-Pro.local>
2023-06-24 02:32:29 -0400

    deploy mumbai nextjs package

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -13,7 +13,7 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The network where your DApp lives in
-  targetNetwork: chains.hardhat,
+  targetNetwork: chains.polygonMumbai,     //CAMBIO NETWORK DEPLOY
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect on the local network
@@ -24,6 +24,7 @@ const scaffoldConfig = {
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
   alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF",
+
 
   // Burner Wallet configuration
   burnerWallet: {
