ffc3a3a3ad24616097fec1b9615b178dbc8d7213
danyaconigriega <dgomezc2@itam.mx>
2023-06-24 13:11:29 -0600

    Editing format of color and font for components

--- packages/nextjs/components/figma/donate-form.tsx
+++ packages/nextjs/components/figma/donate-form.tsx
@@ -11,7 +11,7 @@ const DonateForm: NextPage = () => {
   }, [router]);
 
   return (
-    <div className="w-[1279px] h-[212px] overflow-hidden shrink-0 flex flex-row items-center justify-start gap-[100px] text-center text-16xl text-studio-darkmode-allwhite-ffffff font-inconsolata">
+    <div className="w-[1279px] h-[212px] overflow-hidden shrink-0 flex flex-row items-center justify-start gap-[100px] text-center text-16xl text-studio-darkmode-allwhite-ffffff font-mono">
       <TextField
         className="[border:none] bg-[transparent] flex-1"
         color="primary"
@@ -25,7 +25,7 @@ const DonateForm: NextPage = () => {
         className="rounded-31xl [background:linear-gradient(73.12deg,_#641476,_#9d4561)] w-[294px] h-[117px] flex flex-col py-0 px-[59px] box-border items-end justify-center cursor-pointer"
         onClick={onProfileButtonContainerClick}
       >
-        <div className="relative leading-[40px] font-medium flex items-center justify-center w-[191px] h-11 shrink-0">
+        <div className="relative leading-[40px] font-medium font-mono flex items-center justify-center w-[191px] h-11 shrink-0">
           Donate
         </div>
       </div>
