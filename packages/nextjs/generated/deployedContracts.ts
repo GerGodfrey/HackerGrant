826b880312dbdfdab99220a34fc1d3e8ae3e626e
Gerardo Godfrey <ggc@Gerardos-MacBook-Pro.local>
2023-06-24 02:32:29 -0400

    deploy mumbai nextjs package

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -379,6 +379,244 @@ const contracts = {
       },
     },
   ],
+  80001: [
+    {
+      chainId: "80001",
+      name: "polygonMumbai",
+      contracts: {
+        onlyDonate: {
+          address: "0xd68bA248b0ae7a9A54bCd3d48aB63653D5f00A5a",
+          abi: [
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "new_goal",
+                  type: "uint256",
+                },
+                {
+                  internalType: "uint256",
+                  name: "new_timeout",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "payable",
+              type: "constructor",
+            },
+            {
+              anonymous: false,
+              inputs: [],
+              name: "Cashed",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [
+                {
+                  indexed: false,
+                  internalType: "address",
+                  name: "good_soul",
+                  type: "address",
+                },
+                {
+                  indexed: false,
+                  internalType: "uint256",
+                  name: "donation",
+                  type: "uint256",
+                },
+              ],
+              name: "Donated",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [],
+              name: "Reached",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [],
+              name: "Refunded",
+              type: "event",
+            },
+            {
+              anonymous: false,
+              inputs: [],
+              name: "StartFunding",
+              type: "event",
+            },
+            {
+              inputs: [],
+              name: "donate",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              name: "donations",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "finished_contract",
+              outputs: [
+                {
+                  internalType: "bool",
+                  name: "",
+                  type: "bool",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "goal",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              name: "good_souls",
+              outputs: [
+                {
+                  internalType: "address",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "greeting",
+              outputs: [
+                {
+                  internalType: "string",
+                  name: "",
+                  type: "string",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "is_contract_finished_questionmark",
+              outputs: [
+                {
+                  internalType: "bool",
+                  name: "finito",
+                  type: "bool",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "solicitant",
+              outputs: [
+                {
+                  internalType: "address payable",
+                  name: "",
+                  type: "address",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "bool",
+                  name: "cashing",
+                  type: "bool",
+                },
+              ],
+              name: "stop",
+              outputs: [],
+              stateMutability: "payable",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "time_left",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "time",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "timeout",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              inputs: [],
+              name: "total_raised",
+              outputs: [
+                {
+                  internalType: "uint256",
+                  name: "",
+                  type: "uint256",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+            {
+              stateMutability: "payable",
+              type: "receive",
+            },
+          ],
+        },
+      },
+    },
+  ],
 } as const;
 
 export default contracts;
