e07140692bf9585e5f667d8061f60cb94a9dd719
Diego Jiménez-Vergara <81653270+D9J9V@users.noreply.github.com>
2023-06-24 21:42:25 -0400

    adding carousel-related components

---
+++ packages/nextjs/components/Carousel.tsx
@@ -0,0 +1,18 @@
+import { CarouselProps } from './types';
+import { Card } from './Card'
+
+export const Carousel: React.FC<CarouselProps> = ({ items }) => {
+    return (
+        <div className="overflow-x-scroll whitespace-nowrap py-4 px-2 flex">
+          {items.map((item, index) => (
+            <Card
+              item={item}
+              key={index}
+              //name_title={item.name_title}
+              //imageUrl={item.imageUrl}
+              //progress={item.progress}
+            />
+          ))}
+        </div>
+      );
+};
